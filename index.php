025a4fa25a0b281bd6c9a79fa78479a85f669ab2
gustavo lima <gustavolll15567@gmail.com>
2024-01-29 14:30:45 -0300

    portifolio

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>portifolio Gustavo</title>
+    <link rel="stylesheet" href="css/slick.css">
+    <link rel="stylesheet" href="css/slick-theme.css">
+    <link rel="stylesheet" href="css/estilo.css">
+    <link rel="stylesheet" href="css/resposivo.css">
+</head>
+
+<body>
+    <main>
+        <div class="nome" >
+            <a href="sobremim.php">Gustavo Lima Da Silva Santos</a>
+        </div>
+        <div class="Bem-vindo">
+            <img src="./img/Bem-vindo.png" alt="">
+            <img src="./img/Bem-vindo2.png" alt="">
+
+        </div>
+        <div class="minhafoto">
+            <img src="./img/minhafoto.png" alt="">
+        </div>
+     
+        <div class="buttMeuSite">
+            <button><strong><a href="meusites.php">Meus sites</a></strong></button>
+            <button><strong><a href="sobremim.php">Sobre mim</a></strong></button>
+        </div>
+    </main>
+        <?php require_once ('conteudo/rodape.php') ?>
+</body>
+<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+<script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+<script src="js/slick.min.js"></script>
+<script src="js/script.js"></script>
+
+</html>
---
+++ sobremim.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>portifolio Gustavo</title>
+    <link rel="stylesheet" href="css/slick.css">
+    <link rel="stylesheet" href="css/slick-theme.css">
+    <link rel="stylesheet" href="css/estilo.css">
+    <link rel="stylesheet" href="css/resposivo.css">
+</head>
+
+<body>
+    <section>
+        <div class="sair">
+            <a href="index.php"><svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" fill="currentColor" class="bi bi-box-arrow-left" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M6 12.5a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v2a.5.5 0 0 1-1 0v-2A1.5 1.5 0 0 1 6.5 2h8A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 5 12.5v-2a.5.5 0 0 1 1 0z" />
+                    <path fill-rule="evenodd" d="M.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L1.707 7.5H10.5a.5.5 0 0 1 0 1H1.707l2.147 2.146a.5.5 0 0 1-.708.708z" />
+                </svg></a>
+
+        </div>
+        <div class="topo site">
+            <div class="sobremimFT">
+                <img src="img/minhafoto.png" alt="">
+            </div>
+
+            <div class="sobremimTXT">
+                <h2>Olá! Eu sou Gustavo, tenho 18 anos sou apaixonado por programação. Neste espaço digital, compartilho meu fascínio pelo mundo do código e a jornada emocionante que é explorar e criar com linhas de comando.</h2>
+                <h2> Ao longo desta jornada como programador, tenho dedicado meu tempo a aprimorar minhas habilidades e a desenvolver projetos que refletem minha paixão pela tecnologia. Este portfólio é um vislumbre do que tenho construído até agora.</h2>
+            </div>
+
+        </div>
+
+        <div class="txtlogo site">
+            <div class="logoimg">
+                <img src="./img/CodeCraftPreto.png" alt="">
+            </div>
+            <div class="sobremimTXT" >
+                <h2>A paciência é a chave da tranquilidade enquanto esperamos o que está por vir.</h2>
+            </div>
+
+        </div>
+        <!-- <div>
+            <button><strong><a href="meusites.php">Meus sites</a></strong></button>
+        </div> -->
+    </section>
+    
+    <?php require_once ('conteudo/rodape.php') ?>
+
+</body>
+<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+<script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+<script src="js/slick.min.js"></script>
+<script src="js/script.js"></script>
+
+</html>
