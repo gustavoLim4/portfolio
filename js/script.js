7100c79b8f2af9594995798d323e10a24c299790
gustavo lima <gustavolll15567@gmail.com>
2024-02-03 20:10:00 -0300

    responsivo e mais algumas alterações

--- js/script.js
+++ js/script.js
@@ -7,41 +7,4 @@ $(document).ready(function () {
   });
 });
 
-
-$('.meusSites').slick({
-  dots: true,
-  infinite: true,
-  speed: 300,
-  slidesToShow: 1,
-  adaptiveHeight: true,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: true
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 2
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
-  ]
-
-
-});
+new WOW().init();
