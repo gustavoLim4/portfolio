1a1e774577adadefd57448c04debf7b21c5889b6
gustavoLim4 <gustavolll15567@gmail.com>
2024-02-14 13:25:59 -0300

    mudar fundo gustavo lima e outras ideias

--- js/script.js
+++ js/script.js
@@ -1,10 +1,2 @@
-$(document).ready(function () {
-  $('.Bem-vindo ').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-  });
-});
 
 new WOW().init();
