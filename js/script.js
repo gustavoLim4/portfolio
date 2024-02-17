4facc8dfec9b9c813916f2429f11379c7647b238
gustavoLim4 <gustavolll15567@gmail.com>
2024-02-16 21:26:39 -0300

    alterando no card

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
 new WOW().init();
+
+document.addEventListener("DOMContentLoaded", function() {
+    const openModalBtn = document.getElementById("openModalBtn");
+    const modal = document.getElementById("myModal");
+    const closeBtn = document.getElementsByClassName("close")[0];
+  
+    openModalBtn.addEventListener("click", function() {
+      modal.style.display = "block";
+    });
+  
+    closeBtn.addEventListener("click", function() {
+      modal.style.display = "none";
+    });
+  
+    window.addEventListener("click", function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    });
+  });
+  
