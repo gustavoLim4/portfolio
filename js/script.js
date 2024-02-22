9046d077a8db252cd4bbad56c0bd7f9be94b1c0b
gustavoLim4 <gustavolll15567@gmail.com>
2024-02-22 16:02:59 -0300

    Forma de pagamento de site acionada

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
+$('.imagem').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+});
+	
 new WOW().init();
 
 document.addEventListener("DOMContentLoaded", function() {
